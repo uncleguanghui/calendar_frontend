3e83b0e035eb93fda71f62f817d5f8f7eb816663
uncleguanghui <415558663@qq.com>
2020-05-08 11:07:00 +0800

    添加页面滚动事件监听,实现loadmore功能

--- src/components/Illustration/Illustration.vue
+++ src/components/Illustration/Illustration.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
-    <keep-alive>
-      <a-card>
-        <a-card-grid
-          style="width:25%;text-align:center"
-          v-for="image in files"
-          :key="image"
-        >
-          <img :src="image" alt="" class="card-image" />
-          <div :style="{ paddingTop: '20px' }">
-            {{
-              image
-                .split("/")
-                .pop()
-                .split(".")[0]
-            }}
-          </div>
-        </a-card-grid>
-      </a-card>
-    </keep-alive>
+    <a-card>
+      <a-card-grid
+        style="width:25%;text-align:center"
+        v-for="image in renderFiles"
+        :key="image"
+      >
+        <img :src="image" alt="" class="card-image" />
+        <div :style="{ paddingTop: '20px' }">
+          {{
+            image
+              .split("/")
+              .pop()
+              .split(".")[0]
+          }}
+        </div>
+      </a-card-grid>
+    </a-card>
+    <a-button type="dashed" :style="{ margin: '20px 0' }" @click="loadMore">
+      加载更多
+    </a-button>
   </div>
 </template>
 
@@ -31,18 +32,45 @@ export default {
     const files = require
       .context("../../../public/illustrations", false, /.(png|svg)$/)
       .keys()
-      .map(obj => publicPath + "illustrations/" + obj)
-      .slice(0, 100);
-    console.log(files);
+      .map(obj => publicPath + "illustrations/" + obj);
     return {
-      files: files
+      files: files, //所有图片
+      renderFiles: [], //要渲染的图片
+      pageSize: 40,
+      nextPage: 0,
+      scrollDelta: 100 // 距离底部的阈值，当小于这个值时加载更多图片
     };
   },
   components: {},
-  computed: {},
   beforeMount() {},
-  mounted() {},
-  methods: {},
+  mounted() {
+    // 一开始的时候先加载一次图片
+    this.loadMore();
+    window.addEventListener("scroll", this.scrollListener);
+  },
+  methods: {
+    // 加载更多图片
+    loadMore() {
+      // window.addEventListener("scroll", this.menu);
+      const start = this.nextPage * this.pageSize;
+      this.renderFiles = [
+        ...this.renderFiles,
+        ...this.files.slice(start, start + this.pageSize)
+      ];
+      this.nextPage += 1;
+    },
+    // 滚动的监听事件
+    scrollListener() {
+      const delta =
+        document.documentElement.scrollHeight -
+        document.documentElement.offsetHeight -
+        document.documentElement.scrollTop;
+      if (delta < this.scrollDelta) {
+        this.loadMore();
+      }
+    }
+  },
+  computed: {},
   watch: {}
 };
 </script>
