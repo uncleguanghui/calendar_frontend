ec1506ecb37898cfb7f13b72ef58b07a937ee15e
uncleguanghui <415558663@qq.com>
2020-05-08 12:05:10 +0800

    增加插画模块的搜索功能

--- src/components/Illustration/Illustration.vue
+++ src/components/Illustration/Illustration.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <a-card>
+      <!-- 搜索 -->
+      <a-input-search
+        placeholder="搜索：输入英文关键字"
+        enter-button
+        @search="onSearch"
+        @change="onChange"
+        :style="{ width: '300px' }"
+        slot="title"
+      />
       <!-- 图片展示 -->
       <a-card-grid
-        style="width:25%;text-align:center"
+        :style="{ width: gridWidth, textAlign: 'center' }"
         v-for="image in renderFiles"
         :key="image"
       >
@@ -19,7 +28,12 @@
       </a-card-grid>
     </a-card>
     <!-- 加载更多 -->
-    <a-button type="dashed" :style="{ margin: '20px 0' }" @click="loadMore">
+    <a-button
+      v-if="!searchInput"
+      type="dashed"
+      :style="{ margin: '20px 0' }"
+      @click="loadMore"
+    >
       加载更多
     </a-button>
   </div>
@@ -31,13 +45,17 @@ export default {
   props: [""],
   data() {
     const publicPath = process.env.BASE_URL;
-    const files = require
+    const names = require
       .context("../../../public/illustrations", false, /.(png|svg)$/)
       .keys()
-      .map(obj => publicPath + "illustrations/" + obj);
+      .map(obj => obj.slice(2, obj.length)); //去掉文件名前面的 "./"
     return {
-      files: files, //所有图片
+      publicPath: publicPath,
+      searchInput: "", //搜索文字
+      names: names, //所有图片名
+      files: names.map(obj => publicPath + "illustrations/" + obj), //所有图片路径
       renderFiles: [], //要渲染的图片
+      showFiles: [], //当前已经渲染过的图片，用于取消搜索时加载回来
       pageSize: 40,
       nextPage: 0,
       scrollDelta: 100, // 距离底部的阈值，当小于这个值时加载更多图片
@@ -50,6 +68,7 @@ export default {
   mounted() {
     // 一开始的时候先加载一次图片
     this.loadMore();
+    // 增加滚动监听事件
     window.addEventListener("scroll", this.scrollListener);
   },
   methods: {
@@ -59,6 +78,7 @@ export default {
       const newFiles = this.files.slice(start, start + this.pageSize);
       if (newFiles.length > 0) {
         this.renderFiles = [...this.renderFiles, ...newFiles];
+        this.showFiles = this.renderFiles;
         this.nextPage += 1;
       } else {
         this.notification();
@@ -89,9 +109,43 @@ export default {
       if (delta < this.scrollDelta) {
         this.loadMore();
       }
+    },
+    // 搜索图片
+    onSearch(value) {
+      this.searchInput = value;
+      if (value) {
+        this.renderFiles = this.names
+          .filter(obj => obj.toLowerCase().indexOf(value.toLowerCase()) > -1)
+          .map(obj => this.publicPath + "illustrations/" + obj);
+      } else {
+        this.renderFiles = this.showFiles;
+      }
+    },
+    onChange(e) {
+      const { value } = e.target;
+      this.onSearch(value);
     }
   },
-  computed: {},
+  computed: {
+    gridWidth() {
+      let width;
+      switch (this.renderFiles.length) {
+        case 1:
+          width = "100%";
+          break;
+        case 2:
+          width = "50%";
+          break;
+        case 3:
+          width = "33%";
+          break;
+        default:
+          width = "25%";
+          break;
+      }
+      return width;
+    }
+  },
   watch: {}
 };
 </script>
