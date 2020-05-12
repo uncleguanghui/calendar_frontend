c78c4a2514569001f42e2aaf125364e66d5b87b8
uncleguanghui <415558663@qq.com>
2020-05-12 19:31:39 +0800

    修复了日期无效时 dateformat 函数的返回值异常的问题

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,13 +1,17 @@
 // 日期格式化
 export default function(fmt, date) {
+  if (!date || new Date(date).toDateString() === "Invalid Date") {
+    return "";
+  }
+  let dt = new Date(date);
   let ret;
   const opt = {
-    "Y+": date.getFullYear().toString(), // 年
-    "m+": (date.getMonth() + 1).toString(), // 月
-    "d+": date.getDate().toString(), // 日
-    "H+": date.getHours().toString(), // 时
-    "M+": date.getMinutes().toString(), // 分
-    "S+": date.getSeconds().toString() // 秒
+    "Y+": dt.getFullYear().toString(), // 年
+    "m+": (dt.getMonth() + 1).toString(), // 月
+    "d+": dt.getDate().toString(), // 日
+    "H+": dt.getHours().toString(), // 时
+    "M+": dt.getMinutes().toString(), // 分
+    "S+": dt.getSeconds().toString() // 秒
     // 有其他格式化字符需求可以继续添加，必须转化成字符串
   };
   for (let k in opt) {
