d1454a652dfe2af8db21b576f52f5bd96ff039e6
uncleguanghui <415558663@qq.com>
2020-05-19 14:41:48 +0800

    commit 漏了的全局 svg 加载模块的设置

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  },
   css: {
     loaderOptions: {
       less: {
