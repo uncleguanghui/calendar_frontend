add8f2453357f5bdd1a89d19d60fe9800f4e31d0
uncleguanghui <415558663@qq.com>
2020-05-20 14:08:14 +0800

    对于创建生日任务,增加农历支持

--- src/components/MagicAdd/Event/Birthday.vue
+++ src/components/MagicAdd/Event/Birthday.vue
@@ -21,36 +21,20 @@
         >
         </a-input>
       </a-form-item>
-      <a-form-item label="生日" v-bind="formItemLayout">
-        <a-date-picker
-          v-decorator="['birthday']"
-          placeholder="请选择生日"
-          format="Y年MM月DD日"
-          style="width:100%"
-          @change="
-            e => {
-              selectDate = e;
-            }
-          "
-        >
-        </a-date-picker>
-        <span slot="extra" v-if="leftDays !== null">
-          距离下个生日还有：
-          <span :style="{ color: leftDays === 0 ? 'red' : '#979797' }">
-            {{ leftDays }}
-          </span>
-          天
-        </span>
-      </a-form-item>
       <a-form-item label="类型" v-bind="formItemLayout">
         <a-select
           v-decorator="[
             'dayType',
             {
-              initialValue: 'gregorian'
+              initialValue: selectType
             }
           ]"
           style="width:100%"
+          @change="
+            e => {
+              selectType = e;
+            }
+          "
         >
           <a-select-option value="gregorian">
             公历
@@ -59,6 +43,47 @@
             农历
           </a-select-option>
         </a-select>
+      </a-form-item>
+      <a-form-item label="生日" v-bind="formItemLayout">
+        <a-date-picker
+          :mode="mode"
+          v-decorator="['birthday']"
+          placeholder="请选择生日"
+          :format="dateFormat"
+          style="width:100%"
+          @openChange="handleOpenChange"
+          @panelChange="handlePanelChange"
+          @change="
+            e => {
+              selectDate = e;
+            }
+          "
+        >
+          <template slot="dateRender" slot-scope="current">
+            <!-- 显示农历在日期上 -->
+            <div class="ant-calendar-date" :style="getCurrentStyle(current)">
+              <div class="date-number-string">
+                {{ current.date() }}
+              </div>
+              <div class="date-lunar-string">
+                {{ getLunarString(current) }}
+              </div>
+            </div>
+          </template>
+        </a-date-picker>
+        <span slot="extra" v-if="leftDays !== null">
+          <span v-if="lunarDate && selectType === 'lunar'">
+            {{ $lunar.format(lunarDate, "T A YMd") }}
+            <br />
+          </span>
+          <span>
+            距离下个生日还有：
+            <span :style="{ color: leftDays === 0 ? 'red' : '#979797' }">
+              {{ leftDays }}
+            </span>
+            天
+          </span>
+        </span>
       </a-form-item>
       <a-form-item label="提醒" v-bind="formItemLayout">
         <a-select
@@ -109,27 +134,76 @@
 </template>
 
 <script>
-// TODO: 可以把农历和日历整合在一起
-// TODO: 完善农历下的提醒功能
-// TODO: 完善提醒时间的选择
 export default {
   props: {
     value: Boolean
   },
   computed: {
+    // 农历日期，返回特殊对象 {leap: false, year: 2020, month: 4, day: 1, leapMonth: 4}
+    lunarDate() {
+      if (this.selectDate && this.selectDate._isValid) {
+        let date = new Date(this.selectDate.toDate().toDateString());
+        return this.$lunar.solarToLunar(date); // 生日当天农历（month值在1~12）
+      }
+      return null;
+    },
     // 下一个还没过的生日
     nextBrithday() {
       if (this.selectDate && this.selectDate._isValid) {
         let today = this.$moment().startOf("day"); // 今天
-        let nextBirthday = this.selectDate
-          .clone()
-          .startOf("day")
-          .year(today.get("year"));
-        // 如果今年生日已经过了，则看明年的
-        if (nextBirthday < today) {
-          nextBirthday.add(1, "years");
+        let nextBirthday = this.selectDate.clone().startOf("day"); // 下一个生日
+        if (this.selectType === "lunar") {
+          // 公历转农历，获取农历生日的月、日
+          let lunar = this.lunarDate;
+
+          // 如果生日发生在过去，则切换到今年的生日
+          if (nextBirthday < today) {
+            nextBirthday = this.$moment(
+              this.$lunar.lunarToSolar(
+                today.get("year"),
+                lunar.month,
+                lunar.day
+              )
+            );
+          }
+
+          // 如果今年生日已经过了，则看今年闰月的（leap=true）
+          // 如果没有闰月，则该日期就是上面算的今年生日
+          if (nextBirthday < today) {
+            nextBirthday = this.$moment(
+              this.$lunar.lunarToSolar(
+                today.get("year") + 1,
+                lunar.month,
+                lunar.day,
+                true
+              )
+            );
+          }
+
+          // 如果今年生日已经过了，则看明年的（不用管闰月了）
+          if (nextBirthday < today) {
+            nextBirthday = this.$moment(
+              this.$lunar.lunarToSolar(
+                today.get("year") + 1,
+                lunar.month,
+                lunar.day
+              )
+            );
+          }
+          console.log("下个生日", nextBirthday.format("Y年MM月DD日"));
+        } else {
+          // 如果生日发生在过去，则切换到今年的生日
+          if (nextBirthday < today) {
+            nextBirthday.year(today.get("year"));
+          }
+
+          // 如果今年生日已经过了，则看明年的
+          if (nextBirthday < today) {
+            nextBirthday.add(1, "years");
+          }
+          console.log("下个生日", nextBirthday.format("Y年MM月DD日"));
         }
-        console.log("下个生日", nextBirthday.format("Y年MM月DD日"));
+
         return nextBirthday;
       } else {
         return null;
@@ -149,7 +223,7 @@ export default {
     nextAlarmDays() {
       if (this.nextBrithday && this.selectAlarm !== "none") {
         let today = this.$moment().startOf("day"); // 今天
-        let alarmDate = this.nextCommemoration
+        let alarmDate = this.nextBrithday
           .clone()
           .subtract(parseInt(this.selectAlarm.replace("days", "")), "days"); // 提醒日期
         if (alarmDate >= today) {
@@ -161,11 +235,13 @@ export default {
   },
   data() {
     return {
+      mode: "year",
       form: this.$form.createForm(this),
       formItemLayout: {
         labelCol: { span: 6 },
         wrapperCol: { span: 16 }
       },
+      selectType: "gregorian", // 生日类型
       selectDate: null, // 选择的日期
       selectAlarm: "7days", // 设置的提醒
       newIndex: 0, // 新提醒的序号
@@ -177,7 +253,24 @@ export default {
         { value: "7days", content: "提前7天 ( 8:00 )" },
         { value: "15days", content: "提前15天 ( 8:00 )" },
         { value: "30days", content: "提前30天 ( 8:00 )" }
-      ]
+      ],
+      dateFormat: [
+        "YYYY年MM月DD日",
+        "YYYY年MM月DD",
+        "YYYY-MM-DD",
+        "YYYYMMDD",
+        "YYYY年M月DD日",
+        "YYYY年MM月D日",
+        "YYYY年M月D日",
+        "YYYY年M月DD",
+        "YYYY年MM月D",
+        "YYYY年M月D",
+        "YYYY-M-DD",
+        "YYYY-MM-D",
+        "YYYY-M-D",
+        "YYYYMDD",
+        "YYYYMD"
+      ] // 日期格式化，支持多格式匹配，展示以第一个为准，并以此解析用户的输入项（如复制粘贴进来的日期格式）
     };
   },
   methods: {
@@ -193,9 +286,69 @@ export default {
     },
     handleCancel() {
       this.$emit("input", false);
+    },
+    // 尽可能把字符放到一起，同时如果是农历的初一，则将颜色变红，并增加一个边框
+    getCurrentStyle(current) {
+      let style = {
+        width: "38px",
+        height: "38px",
+        lineHeight: 1,
+        paddingTop: "6px"
+      };
+      // 判断农历是否为初一
+      let date = new Date(current.toDate().toDateString());
+      let lunar = this.$lunar.solarToLunar(date);
+      if (lunar.day === 1) {
+        style.color = "#ff6464";
+        style.border = "1px solid #ff6464";
+        style.borderRadius = "50%";
+      }
+      return style;
+    },
+    // 获取农历字符串
+    // 因为 lunar 库有点问题，当小时和分钟不为0时，给出的农历日期是有问题的，所以这里稍作处理
+    getLunarString(current, format = "D") {
+      let date = new Date(current.toDate().toDateString());
+      let lunar = this.$lunar.solarToLunar(date);
+      return this.$lunar.format(lunar, format);
+    },
+    // 禁止选择的日期为未来（暂时不限制了。。体验上并没有多好）
+    disabledDate(current) {
+      return current && current > this.$moment().endOf("day");
+    },
+    // 打开面板时，如果已输入了有效日期，则进入日期页面，否则重置 mode 为 year
+    handleOpenChange(open) {
+      if (open) {
+        if (this.selectDate) {
+          this.mode = "date";
+        } else {
+          this.mode = "year";
+        }
+      }
+    },
+    // 在面板上点击时，触发此面板切换函数
+    // value 为 一个 moment 对象
+    handlePanelChange(value, mode) {
+      if (!mode) {
+        // 当在 year 面板上点击时（不知道为什么 mode 为 null），切换到 month
+        this.mode = "month";
+      } else {
+        // 当在 month 面板上点击时（不知道为什么 mode 不是 month 而是 date），切换到 date
+        this.mode = "date";
+      }
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.date-number-string {
+  font-size: 12px;
+}
+
+/* 农历字符在变成最小的12px的同时，再进行0.7倍的缩小 */
+.date-lunar-string {
+  transform: scale(0.6);
+  font-size: 12px;
+}
+</style>
